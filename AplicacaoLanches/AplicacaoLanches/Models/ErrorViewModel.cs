5d4ac8a992d10d9e455f581b882a35273ce4f4b7
Felipe <felipe.mitre16@gmail.com>
2023-11-30 11:18:05 -0300

    Enviando inicio do projeto

---
+++ AplicacaoLanches/AplicacaoLanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplicacaoLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
